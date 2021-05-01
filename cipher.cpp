25ef5eb00b484bec711e1119c36eb027f004d3b2
Carlos Garcia <carlosgarcia10001@csu.fullerton.edu>
2021-05-01 04:05:54 -0700

    Big Fix

--- cipher.cpp
+++ cipher.cpp
@@ -7,18 +7,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	/**
-	 * TODO: Replace the code below	with your code which can SWITCH
-	 * between DES and AES and encrypt files. DO NOT FORGET TO PAD
-	 * THE LAST BLOCK IF NECESSARY.
-	 *
-	 * NOTE: due to the incomplete skeleton, the code may crash or
-	 * misbehave.
-	 */
-	
-	/* Create an instance of the DES cipher */	
-	CipherInterface* cipher = new DES(); 
-		
+
+	//UserInput user = get_user_input(argc, argv);
+	CipherInterface* cipher;
 	/* Error checks */
 	if(!cipher)
 	{
@@ -26,15 +17,27 @@ int main(int argc, char** argv)
 		__FILE__, __FUNCTION__, __LINE__);
 		exit(-1);
 	}
-	
+	File* fp = fopen(user.input_file_name);
+	char buffer[8];
+
+	if(
+		//typeid(user.cipher).name()=="DES" 
+	)
+	{
+		cipher = new DES();
+	}
+	else
+	{
+		cipher = new AES();
+		addEncryptionFlag(user.key, user.encrypt);
+	}
 	/* Set the encryption key
 	 * A valid key comprises 16 hexidecimal
 	 * characters. Below is one example.
 	 * Your program should take input from
 	 * command line.
 	 */
-	cipher->setKey((unsigned char*)"0123456789abcdef");
-	
+	cipher->setKey(user.key);
 	/* Perform encryption */
 	unsigned char* cipherText = cipher->encrypt((unsigned char*)"hello world");
 	
@@ -42,4 +45,18 @@ int main(int argc, char** argv)
 	//cipher->decrypt(cipherText);	
 	
 	return 0;
+}
+
+void addEncryptionFlag(char *aes_key, bool encrypt){
+	char *marked_aes_key (unsigned char*)malloc(17*sizeof(unsigned char));;
+	if(encrypt){
+		marked_aes_key[0] = '00';
+	}
+	else{
+		marked_aes_key[0] = '01';
+	}
+	for(int i = 1; i < 17; i++){
+		marked_aes_key[i] = aes_key[i-1];
+	}
+	aes_key = marked_aes_key;
 }
